4abf56ea7a3866a0b99f9c80c43206b5d5847467
Pablete1234 <pabloherrerapalacio@gmail.com>
2016-11-25 07:38:53 +0100

    Make head placement inteligent and persistent

--- src/main/java/me/pablete1234/arsmea/modules/HeadDrop.java
+++ src/main/java/me/pablete1234/arsmea/modules/HeadDrop.java
@@ -1,5 +1,9 @@
 package me.pablete1234.arsmea.modules;
 
+import com.google.common.collect.Lists;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import me.pablete1234.arsmea.Arsmea;
 import me.pablete1234.arsmea.ListenerModule;
 import me.pablete1234.arsmea.util.Config;
@@ -18,23 +22,72 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.material.MaterialData;
 import org.bukkit.util.BlockVector;
+import org.bukkit.util.Vector;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class HeadDrop implements ListenerModule {
 
-    private Map<BlockVector, BlockState> removedBlocks = new HashMap<>();
+    private static final BlockFace[] FACES = {BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST, BlockFace.UP, BlockFace.DOWN};
+    private static final List<Material> nonUsable = Arrays.asList(
+            Material.ACACIA_DOOR,
+            Material.BIRCH_DOOR,
+            Material.DARK_OAK_DOOR,
+            Material.JUNGLE_DOOR,
+            Material.SPRUCE_DOOR,
+            Material.WOODEN_DOOR,
+            Material.IRON_DOOR_BLOCK);
+
+    private static final String REMOVED_BLOCKS_FILE = "HeadDrop_removedBlocks.json";
+    private static final Gson gson = new GsonBuilder().enableComplexMapKeySerialization().create();
+
+    private Map<BlockVector, MaterialData> removedBlocks;
+
+    @Override
+    public void load() {
+        File input = new File(Arsmea.instance().getDataFolder().getPath() + File.separator + REMOVED_BLOCKS_FILE);
+        if (input.exists()) {
+            try {
+                removedBlocks = gson.fromJson(new FileReader(input), new TypeToken<Map<BlockVector, MaterialData>>(){}.getType());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (removedBlocks == null) removedBlocks = new HashMap<>();
+    }
+
+    @Override
+    public void unload() {
+        File output = new File(Arsmea.instance().getDataFolder().getPath() + File.separator + REMOVED_BLOCKS_FILE);
+        try {
+            output.delete();
+            Files.write(output.toPath(), gson.toJson(removedBlocks).getBytes(), StandardOpenOption.CREATE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlayerDeath(PlayerDeathEvent event) {
         if (!Config.dropHeadsOnDeath) return;
         Player p = event.getEntity();
-        if (Config.placeHeadOnDeath) {
-            Location loc = p.getLocation().add(0, 0.25, 0);
-            Block block = loc.getBlock();
-            removedBlocks.put(toBlockVector(loc), block.getState());
+        Block block = null;
+        if (Config.placeHeadOnDeath)
+            block = findUsableBlock(p.getLocation().getBlock());
+        if (block != null) {
+            if (!block.getType().equals(Material.AIR))
+                removedBlocks.put(toBlockVector(block), block.getState().getData());
             block.setType(Material.SKULL);
             Skull state = (Skull) block.getState();
             state.setSkullType(SkullType.PLAYER);
@@ -55,13 +108,13 @@ public class HeadDrop implements ListenerModule {
     public void onPlayerBreakBlock(BlockBreakEvent event) {
         BlockVector loc = toBlockVector(event.getBlock().getLocation());
         if (removedBlocks.containsKey(loc)) {
-            BlockState oldState = removedBlocks.get(loc);
+            MaterialData oldData = removedBlocks.get(loc);
             Block block = event.getBlock();
             removedBlocks.remove(loc);
             Bukkit.getScheduler().scheduleSyncDelayedTask(Arsmea.instance(), () -> {
-                block.setType(oldState.getType());
+                block.setType(oldData.getItemType());
                 BlockState newState = block.getState();
-                newState.setData(oldState.getData());
+                newState.setData(oldData);
                 newState.update();
             });
         }
@@ -69,6 +122,46 @@ public class HeadDrop implements ListenerModule {
 
     private static BlockVector toBlockVector(Location location) {
         return new BlockVector(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+    }
+
+    private static BlockVector toBlockVector(Block block) {
+        return toBlockVector(block.getLocation());
+    }
+
+    private Block findUsableBlock(Block source) {
+        if (isUsable(source)) return source;
+        List<Block> nextScan = Lists.newLinkedList();
+        List<Vector> alreadyScanned = Lists.newLinkedList();
+        nextScan.add(source);
+        alreadyScanned.add(source.getLocation().toVector());
+        return findUsableBlock(nextScan, alreadyScanned);
+    }
+
+    private Block findUsableBlock(List<Block> scan, List<Vector> alreadyScanned) {
+        if (scan.size() == 0) return null;
+        List<Block> nextScan = new LinkedList<>();
+        for (Block scanning : scan) {
+            if (alreadyScanned.size() > 1024) {
+                Bukkit.getConsoleSender().sendMessage("Maximim scan area (1024 blocks) was reached.");
+                return null;
+            }
+            for (BlockFace face : FACES) {
+                Block block = scanning.getRelative(face);
+                if (alreadyScanned.contains(block.getLocation().toVector())) continue;
+                if (isUsable(block)) {
+                    return block;
+                } else {
+                    nextScan.add(block);
+                    alreadyScanned.add(block.getLocation().toVector());
+                }
+            }
+        }
+        return findUsableBlock(nextScan, alreadyScanned);
+    }
+
+    private boolean isUsable(Block block) {
+        return block.getState().getClass().getName().endsWith("CraftBlockState")
+                && !nonUsable.contains(block.getType());
     }
 
 }
